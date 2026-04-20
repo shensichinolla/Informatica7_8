a7970becc69471889e0a77b7bbe68cee0452b697
Shensi Chinolla <chinollashensi@gmail.com>
2026-04-20 17:19:41 +0000

    index.html

--- js-events/script.js
+++ js-events/script.js
@@ -1,16 +1,18 @@
 //function greet(name){
     // console.log("Hello"+ name);
-}
+//}
 // greet("Nose");
 
 
 
-function articlesFaith(article1,article2,article3){
-    article1= We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.;
-    article2= We believe that men will be punished for their own sins, and not for Adam’s transgression.;
-    article3= We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.;
-
-    alert("Welcome to Articles of Faith! ");
+function articleFaith1(){
+    alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.");
+}
+function articleFaith2(){
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.");
+}
+function articleFaith3(){
+    alert("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.");
 }
 
 //addNumbers(6,7)
