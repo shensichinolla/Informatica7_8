e39e49808cebdc325c224df1876e875e658048f1
Shensi Chinolla <chinollashensi@gmail.com>
2026-04-22 20:19:10 +0000

    index.html

---
+++ js-variables/script.js
@@ -0,0 +1,21 @@
+function function1(){
+    const firstName="Shensi";
+    const favColor="Pink";
+
+    let location ="Colonia Juarez";
+    let mood= "Tired";
+
+    console.log("Name:"+ firstName);
+    console.log("Favorite Color:"+ favColor);
+    console.log("Location:"+ location);
+    console.log("Mood:"+ mood);
+
+    mood= "Sleepy"
+
+}
+function function2(){
+    let companyName="Nike";
+    let foundingYear= 1964
+    let isActive= true;
+    
+}
