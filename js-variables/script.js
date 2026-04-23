49a1b3e1058511289b66a6e76d43eeaa657a53d4
Shensi Chinolla <chinollashensi@gmail.com>
2026-04-23 17:07:24 +0000

    index.html

--- js-variables/script.js
+++ js-variables/script.js
@@ -11,11 +11,28 @@ function function1(){
     console.log("Mood:"+ mood);
 
     mood= "Sleepy"
+    console.log("New Mood:"+mood);
 
 }
 function function2(){
     let companyName="Nike";
     let foundingYear= 1964
     let isActive= true;
-    
+    let fundingAmount;
+
+    console.log("Company Name:"+ companyName);
+    console.log("Founding Year:" + foundingYear);
+    console.log("Is Active:" + isActive);
+    console.log("Funding Amount:"+ fundingAmount);
+
+}
+function function3(){
+    let variable=5;
+
+    variable= variable * 2;
+    variable= variable + 10;
+    variable=variable / 2;
+    variable= variable - 5;
+
+    alert("The final value is: "+ variable);
 }
