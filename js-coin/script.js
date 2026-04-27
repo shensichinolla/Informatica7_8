2e399d5d1caed233d8a65758040ac547c561d02f
Shensi Chinolla <chinollashensi@gmail.com>
2026-04-27 20:20:15 +0000

    index.html

---
+++ js-coin/script.js
@@ -0,0 +1,14 @@
+function flipcoin(){
+    let randomNumber= Math.random();
+    console.log(randomNumber);
+
+    let subtitle= document.querySelector("h2");
+    if (randomNumber < 0.5){
+        subtitle.innerHTML="Heads";
+        subtitle.style.color="red";
+    }else{
+        subtitle.innerHTML="Tails";
+        subtitle.style.color="blue";
+    }
+
+}
