e09607a46513308e2e9bc364f4e1d54051375a3d
Shensi Chinolla <chinollashensi@gmail.com>
2026-05-05 16:28:59 +0000

    index.html

--- test-rps/script.js
+++ test-rps/script.js
@@ -1,14 +1,33 @@
-function function1{
-    const options= rock, paper, scissors;
-    const choice= Math.random()*3;
+function game(userChoice){
+    let randomNumber=Math.random();
+    let compChoice;
+    if (randomNumber < 0.33){
+        compChoice="Rock";
+    }else if(randomNumber < 0.66){
+        compChoice="Paper";
+    }else if(randomNumber < 1){
+        compChoice="Scissors";
+    }
 
+    let result = document.querySelector
+    ("#result");
+    if (userChoice == compChoice){
+        result.innerHTML="Tie";
+    }else if (userChoice == "Rock"){
+        if (compChoice == "Paper"){
+            result.innerHTML="YOU LOOSE!";
+        }else {
+            result.innerHTML="YOU WIN";
+        }
+    }
 
-let result = document.querySelector("h3");
-if(user == "Rock"){
-    result.innerHTML = "You won";
-}else{
-    result.innerHTML = "You lost";
-}else{
-    result.innderHTML= "Tie"
+    console.log(compChoice)
+}
+
+//document.addEventListener('DOMContentLoaded', function(){
+    //code here
+    //document.querySelector("#rock-button").onclick=
+
+    });
 }
 
