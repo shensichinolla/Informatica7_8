652ac909acdea02f13b788397e6a164893c147a7
Shensi Chinolla <chinollashensi@gmail.com>
2026-05-13 19:58:32 +0000

    index.html

--- intervals/script.js
+++ intervals/script.js
@@ -8,6 +8,14 @@ counter--;
 let minutes = Math.floor(counter / 60);
 let seconds = counter % 60;
 
+if (seconds < 10){
+    seconds= '0' + seconds;
+}
+
+if (minutes < 10){
+    minutes ='0'+ minutes;
+}
+documentquerySelector('h1').innerHTML= minutes 
 
 
 
